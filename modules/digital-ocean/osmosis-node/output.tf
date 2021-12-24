37b0354a510f4b58573023f9ec8ab43c2ed9c2e8
Niccolo Raspa <nraspa@live.it>
2021-12-24 09:09:27 +0100

    Add output description

--- modules/digital-ocean/osmosis-node/output.tf
+++ modules/digital-ocean/osmosis-node/output.tf
@@ -6,16 +6,20 @@ locals {
 
 output "nodes_names" {
   value = local.nodes_names
+  description = "Name(s) of of the osmosis node(s)"
 }
 
 output "nodes_ips" {
   value = local.nodes_ips
+  description = "IP(s) of of the osmosis node(s)"
 }
 
 output "nodes_info" {
   value = zipmap(local.nodes_names, local.nodes_ips)
+  description = "Info about the osmosis node(s)"
 }
 
 output "tls_private_key" {
   value = local.created_tls_key ? tls_private_key.osmosis_tls_key.0.private_key_pem : file(var.tls_private_key)
+  description = "TLS private key to use to connect to the osmosis nodes"
 }
