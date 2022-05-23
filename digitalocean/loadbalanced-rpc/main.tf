b482d80b7856128b3a514794bbd8f516573bc722
Niccolo Raspa <nraspa@live.it>
2022-05-23 14:56:34 +0200

    Update examples

--- digitalocean/loadbalanced-rpc/main.tf
+++ digitalocean/loadbalanced-rpc/main.tf
@@ -21,23 +21,14 @@ terraform {
 module "osmosis-rpc" {
   source = "../../modules/digitalocean/rpc"
 
-  name   = "osmosis-single-rpc"
-  nodes  = 1
+  name   = "osmosis-loadbalanced-rpc"
+  nodes  = 3
   region = "nyc3"
   tags   = ["osmosis", "fullnode"]
 
   # This is the recommended size for mainnet
   # Find others slugs with: `doctl compute size list`"
   size = "m3-4vcpu-32gb"
-}
-
-resource "digitalocean_floating_ip" "this" {
-  region = "nyc3"
-}
-
-resource "digitalocean_floating_ip_assignment" "this" {
-  ip_address = digitalocean_floating_ip.this.ip_address
-  droplet_id = module.osmosis-rpc.droplets_ids[0]
 }
 
 # ------------------------------------------------------------
